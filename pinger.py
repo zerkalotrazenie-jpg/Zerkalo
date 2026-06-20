ff2e26e0be2e0885bebb85b890ba756d959d2597
zerkalotrazenie-jpg <zerkalotrazenie@gmail.com>
2026-06-20 06:20:14 +0500

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -10,11 +10,18 @@ def super_pinger():
         try:
             response = requests.get(url, timeout=10)
             ping_count += 1
-            print(f"🔵 Пинг #{ping_count} | Статус: {response.status_code}")
+            print(f"🔵 ПИНГ #{ping_count} | Статус: {response.status_code}")
         except:
             pass
-        time.sleep(120)
+        time.sleep(60)
 
 def start_pinger():
     thread = threading.Thread(target=super_pinger, daemon=True)
     thread.start()
+    print("✅ ПИНГ ЗАПУЩЕН")
+
+# Если файл запущен напрямую
+if __name__ == "__main__":
+    start_pinger()
+    while True:
+        time.sleep(60)
